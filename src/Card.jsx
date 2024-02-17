185807fb08a787b04e720decbee3062067346548
printMelen <alvaro.redondofgb@gmail.com>
2024-02-17 23:01:11 +0100

    Mostrando la imagen

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 const Card = ({data}) => {
   return (
     <div className='w-[600px] mx-auto mt-5'>
-        <div className='bg-blue-500 text-white p-2 rounded-lg'>
+        <div className='border text-white p-2 rounded-lg'>
+          <img src={"http://openweathermap.org/img/w/"+data.weather[0].icon+".png"}/>
             <h2>{data.name}</h2>
             <p>Temperatura: {data.main.temp} ºC</p>
+            <p>Temperatura Max: {data.main.temp_max} ºC</p>
+            <p>Temperatura Min: {data.main.temp_min} ºC</p>
             <p>Clima: {data.weather[0].description}</p>
-            <p>Humedad: 20%</p>
-            <p>Viento: 10 km/h</p>
+            <p>Humedad: {data.main.humidity}%</p>
+            <p>Viento: {data.wind.speed} km/h</p>
         </div>
     </div>
   )
